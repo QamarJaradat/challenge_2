aa478e4df8ccfb45e7246c1f6efa288506667228
QamarJaradat <moon19.qs@gmail.com>
2020-11-03 20:00:18 +0200

    bye bye challenge2

--- client/app.js
+++ client/app.js
@@ -4,7 +4,6 @@ $('#selectedfile').on('change', (e) => {
     var file = e.target.files[0];
     var reader = new FileReader();
     reader.onload = function (e) {
-        // console.log(e.target.result)
         $('#filetext').val(e.target.result)
     };
     reader.readAsText(file);
@@ -13,40 +12,50 @@ $('#selectedfile').on('change', (e) => {
 $('form').on('submit', (e) => {
     e.preventDefault();
     var value = $('#filetext').val()
-    console.log("i'm here")
-
-    $.ajax({
-        method: "POST",
-        url: "/",
-        data: { data: value },
-        success: function (res) {
-            console.log("its work")
-            $('.csvReport').text(res)
-            array = $('.csvReport').text().split('\n')
-        },
-        error: function (error) {
-            console.error('Failed to fetch files', error);
-        }
-    })
-    //reset the choosen file and the textarea
-    $('#filetext').val("")
-    $('#selectedfile').val('')
-    $('#buttonfilter').show()
-    $('#testfilter').show()
-
+    if (value === '') {
+        alert('choose a file or enter text in the text area!!')
+    }
+    else {
+        $.ajax({
+            method: "POST",
+            url: "/",
+            data: { data: value },
+            success: function (res) {
+                console.log("its work")
+                $('.csvReport').text(res)
+                array = $('.csvReport').text().split('\n')
+            },
+            error: function (error) {
+                console.error('Failed to fetch files', error);
+            }
+        })
+        //reset the choosen file and the textarea
+        $('#filetext').val("")
+        $('#selectedfile').val('')
+        $('#buttonfilter').show()
+        $('#testfilter').show()
+    }
 
 })
 
 $('#buttonfilter').on('click', () => {
     var filtervalue = $('#testfilter').val()
     var temparray = array.slice()
-    for (var i = 0; i < temparray.length; i++) {
+    var count = 1
+    for (var i = 1; i < temparray.length; i++) {
         if (temparray[i].includes(filtervalue)) {
             temparray.splice(i, 1)
             i--
         }
+        else {
+            temparray[i] = replaceAt(temparray[i], 0, count)
+            count++
+        }
     }
-    console.log(temparray)
-    console.log(array)
-
+    $('.csvReport').text(temparray.join('\n'))
 })
+
+
+function replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
+}
--- server.js
+++ server.js
@@ -8,31 +8,20 @@ app.use(bodyparser.urlencoded({
 }));
 app.use(bodyparser.json())
 app.use(express.static('client'))
-var headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': '*',
-    'Content-type': 'application/json'
-}
 
 
 var convertedfile
 app.post('/', (req, res) => {
     console.log("got post request")
-    // var obj = JSON.parse(req.body.data)
-    // console.log((req.body.data))
     // res.status(201)
-    // res.set(headers)
     convertedfile = jsonToCSV(req.body.data)
-    // console.log(convertedfile)
     res.send(convertedfile)
 })
-
 
 app.listen(3000)
 console.log('lisening on 3000')
 
 var jsonToCSV = (obj) => {
-    // return "good job"
     obj = JSON.parse(obj)
     var stringresult = ''
     stringresult += Object.keys(obj).slice(0, -1).join(',')
@@ -49,12 +38,9 @@ var jsonToCSV = (obj) => {
             });
         }
     }
+
     find(obj)
-
-
     return stringresult
-
-
 }
 
 
