646822ca6b6efeb606e82ca86f72b5125ac1db58
QamarJaradat <moon19.qs@gmail.com>
2020-11-03 12:38:23 +0200

    read file

--- client/app.js
+++ client/app.js
@@ -1,2 +1,29 @@
+// var $ = require("jquery");
+// var reader = new FileReader();
+// reader.addEventListener("loadend", function () {
+//     console.log(reader.result)
+//     // document.getElementById('filetext').innerText = reader.result;
+// });
 
+// console.log(file)
+// reader.readAsText(file);
 
+// const reader = new FileReader();
+// reader.addEventListener('load', (event) => {
+//     console.log(event.target.result)
+// });
+
+// reader.readAsText(file);
+
+var selectedFile = document.getElementById("selectedfile")
+$('#selectedfile').on('change', (e) => {
+
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        // console.log(e.target.result)
+        $('#filetext').val(e.target.result)
+    };
+
+    reader.readAsText(file);
+})
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express')
-var bodyparser = require(body_parser)
+var bodyparser = require('body-parser')
 var app = express()
 
 app.use(bodyparser.json())
 app.use(express.static('client'))
+
+app.listen(3000)
+console.log('lisening on 3000')
+
+
+app.post('/', (req, res) => {
+    console.log("got post request")
+})
 
